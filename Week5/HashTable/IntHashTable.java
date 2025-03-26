9883faf262f3a02706c3b36e175330f2fe096b3b
Damion <ds526@students.waikato.ac.nz>
2025-03-26 16:57:21 +1300

    week 5 lecture 2(go back to watch lecture ERRORS)

--- Week5/HashTable/IntHashTable.java
+++ Week5/HashTable/IntHashTable.java
@@ -1,10 +1,22 @@
 public class IntHashTable {
     Node[] hashTable;
-    int size;
+    int size, numFullEntries, numCollisions, numRehashes;
 
     public IntHashTable(int size) {
         hashTable = new Node[size];
         this.size = size;
+    }
+
+    public void rehash() {
+        Node[] temp = hashTable;
+        size *= 2;
+        hashTable = new Node[size];
+
+        for(int i = 0; i < temp.length; i++) {
+            Node t = temp[i];
+            insert(t.k, t.v);
+            numFullEntries--;
+        }
     }
 
     public int getHash (int k) {
@@ -15,18 +27,35 @@ public class IntHashTable {
         int index = getHash(k);
         hashTable[index] = new Node(k, v);
 
-        if (hashTable[index] == null) {
-            hashTable[index] = new Node(k, v);
-        } else {
-            System.out.println("Collision detected at index " + index + " for key " + k);
-            // Handle collision (e.g., linear probing, chaining, etc.)
+        if (numFullEntries >= size) {
+            System.out.println("Rehashing required, current size: " + size);
+            rehash();
+            numRehashes++;
         }
+
+        // if (hashTable[index] == null) {
+        //     hashTable[index] = new Node(k, v);
+        //     numFullEntries++;
+        // } else {
+        //     System.out.println("Collision detected at index " + index + " for key " + k);
+        //     numCollisions++;
+        // }
+
+        int i = 0;
+        while(hashTable[(index + i) % size] != null) {
+            i++;
+
+        }
+
+        hashTable[(index + i) % size] = new Node(k, v);
+        numFullEntries++;
     }
 
     public void delete(int k) {
         int index = getHash(k);
         if (hashTable[index] != null && hashTable[index].k == k) {
             hashTable[index] = null;
+            numFullEntries--;
         } else {
             System.out.println("Key " + k + " not found for deletion.");
         }
--- Week5/HashTable/Main.java
+++ Week5/HashTable/Main.java
@@ -2,14 +2,19 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("Printing Hash Table");;
 
-        IntHashTable hashTable = new IntHashTable(5);
+        IntHashTable hashTable = new IntHashTable(3);
         hashTable.print();
 
         hashTable.insert(1, 10);
         hashTable.insert(2, 20);
+        hashTable.insert(908234, 30);
+        
+        System.out.println();
         hashTable.print();
 
-        hashTable.delete(1);
+        hashTable.insert(1, 100); // This should cause a collision
+        hashTable.insert(4, 40);
+        System.out.println();
         hashTable.print();
 
         
